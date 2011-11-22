1e43ff2b20927737e052ac00ead91f2340ad7c88
Dani <dani@shiryu.(none)>
2011-11-22 23:10:01 +0100

    javascript aparte

---
+++ public/javascripts/home.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('td').mouseover(function(){
+        if ($(this).html() != '') {
+            var m = $(this).parents('table').children('caption').html();
+            $('h1').html( $(this).html() + ' de ' + m );
+            $('h5').html( $(this).attr('title') );
+        }
+    });
+    $('td').click(function(){
+        var dia = $(this).attr('data-dia');
+        actividad = prompt('Actividad (1 a 4) realizada el ' + dia);
+        if (actividad == null || actividad == '') return;
+        resumen = prompt('Resumen del ' + dia);
+        if (resumen == null || resumen == '') return;
+        $('#hoy').val(dia + "\t" + actividad + "\t" + resumen);
+        document.forms[0].submit();
+    });
+});
