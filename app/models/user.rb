30d200a6c720b4a88c43496198280414a462f335
Dani <dani@shiryu.(none)>
2012-12-13 19:27:06 +0100

    fix password typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :email, :clave, :presence => true
+  validates :email, :password, :presence => true
   validates :email, :uniqueness => true
   
   attr_accessible :email, :password, :can_download, :can_edit_activity, :can_edit_event, :can_see_legend, :can_see_description
--- lib/tasks/test.rake
+++
@@ -1,9 +0,0 @@
-Rake.application.remove_task 'db:test:prepare'
-
-namespace :db do
-  namespace :test do
-    task :prepare do |t|
-      # rewrite the task to not do anything you don't want
-    end
-  end
-end
