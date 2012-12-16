bed9e3e0179f5ad4c5a7e549fafa59c0992d3388
Dani <dani@shiryu.(none)>
2012-12-16 21:45:41 +0100

    password management is now easier with Clearance, removed my old controller code

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,48 +11,24 @@ class Admin::UsersController < AdminController
 
   def create
     @user = User.new(params[:user])
-    @user.password = Digest::MD5.hexdigest(@user.password) unless @user.password.blank?
 
     if @user.save
       redirect_to admin_users_path, :notice => 'User created'
     else
-      @user.password = ''
       render 'new'
     end
   end
 
   def edit
     @user = User.find(params[:id])
-
-    redirect_to admin_users_path, :alert => 'User cannot be edited' and return if @user.email == 'guest'
-
-    @user.password = ''
   end
 
   def update
     @user = User.find(params[:id])
 
-    redirect_to admin_users_path, :alert => 'User cannot be edited' and return if @user.email == 'guest'
-
-    if @user.email == 'admin'
-      params[:user] = {:password => params[:user][:password]} #the only field allowing modification is password
-    end
-
-    password = params[:user][:password]
-    if password.blank? #if password is left blank, we don't update it
-      params[:user].delete :password
-    else #if password is set, we modify current password
-      params[:user][:password] = Digest::MD5.hexdigest(password)
-    end
-
     if @user.update_attributes(params[:user])
-      if password.blank? and @user.email == 'admin'
-        redirect_to admin_users_path, :notice => 'User not modified'
-      else
-        redirect_to admin_users_path, :notice => 'User updated'
-      end
+      redirect_to admin_users_path, :notice => 'User updated'
     else
-      @user.clave = ''
       render 'edit'
     end
   end
