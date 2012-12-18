8c6d44e3b5af814e7788154277d222f231c8fce3
Dani <dani@shiryu.(none)>
2012-12-18 23:15:08 +0100

    unit tests for Activity

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Activity do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'has a valid factory' do
+    FactoryGirl.build(:activity).should be_valid
+  end
+
+  it 'is invalid without name' do
+    FactoryGirl.build(:activity, name: nil).should_not be_valid
+  end
+
+  it 'is invalid without color, or improper color format' do
+    FactoryGirl.build(:activity, color: nil).should_not be_valid
+
+    FactoryGirl.build(:activity, color: 'deadbeef').should_not be_valid
+
+    FactoryGirl.build(:activity, color: '#whatever').should_not be_valid
+
+    FactoryGirl.build(:activity, color: '12345').should_not be_valid
+  end
+  
 end
