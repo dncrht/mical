505356f85cda580bb41a49214fe2f9d62add31c0
Dani <dani@shiryu.(none)>
2012-12-18 23:42:28 +0100

    unit tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'has a valid factory' do
+    FactoryGirl.build(:user).should be_valid
+  end
+
+  it 'is invalid without email, or invalid email' do
+    FactoryGirl.build(:user, email: nil).should_not be_valid
+    
+    FactoryGirl.build(:user, email: 'nil@').should_not be_valid
+  end
+  
+  it 'is invalid without password' do
+    FactoryGirl.build(:user, password: nil).should_not be_valid
+  end
+  
+  it 'accept a blank password if the user exists' do
+    user = FactoryGirl.create(:user)
+    user.save
+    user.password = ''
+    user.should be_valid
+  end
+  
+  it 'is invalid if the email exists' do
+    FactoryGirl.create(:user)
+    FactoryGirl.build(:user).should_not be_valid
+  end
+  
+  it "can't delete the last admin" do
+    user = FactoryGirl.create(:user)
+    expect { user.destroy }.to raise_error 'Must be at least one admin'
+  end
+  
 end
