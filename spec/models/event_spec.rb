9b964aa919baa6b1745f87508a18243975574823
Dani <dani@shiryu.(none)>
2012-12-20 22:09:28 +0100

    additional tests for Event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,6 +2,30 @@ require 'spec_helper'
 
 describe Event do
 
+  it 'has a valid factory' do
+    FactoryGirl.build(:event).should be_valid
+  end
+
+  it 'is invalid without day' do
+    FactoryGirl.build(:event, day: nil).should_not be_valid
+  end
+  
+  it 'day should be unique' do
+    FactoryGirl.create(:event)
+    
+    event = FactoryGirl.build(:event) # Same event
+    event.should_not be_valid
+    event.should have(1).error_on(:day)
+  end
+  
+  it 'is invalid without activity' do
+    FactoryGirl.build(:event, activity_id: nil).should_not be_valid
+  end
+  
+  it 'is invalid without description' do
+    FactoryGirl.build(:event, description: nil).should_not be_valid
+  end
+  
   it 'should not replace without day' do
     Event.replace(nil, nil, nil).should be_nil
   end
