122018f2b5b62e0dae8ef2fe0446dddff105a818
Dani <dani@shiryu.(none)>
2012-12-23 10:57:51 +0100

    change is_admin from user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   include Clearance::User
   
-  attr_accessible :email, :password, :can_download, :can_edit_activity, :can_edit_event, :can_see_legend, :can_see_description
+  attr_accessible :email, :password, :can_download, :can_edit_activity, :can_edit_event, :can_see_legend, :can_see_description, :is_admin
   
   before_destroy :at_least_one_admin
   
