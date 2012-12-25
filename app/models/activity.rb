1f450914a8b44301ab859e604a009dae4e6fd485
Dani <dani@shiryu.(none)>
2012-12-25 23:00:13 +0100

    set up an _on delete cascade_ in rails associations, to avoid orphan events in case the trigger is not set at database level

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,5 +2,7 @@ class Activity < ActiveRecord::Base
   validates :name, :presence => true
   validates :color, :format => /^#[0-9A-Fa-f]{6}$/
   
+  has_many :events, :dependent => :destroy
+  
   attr_accessible :name, :color
 end
