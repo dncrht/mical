d227ee9abc2ce0ee318e7b02ea30f40f9152910a
Dani <dani@shiryu.home>
2013-08-02 20:04:12 +0100

    Better error messages on form fields

--- config/initializers/my_error_spans.rb
+++ config/initializers/my_error_spans.rb
@@ -1,15 +1,13 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  if instance.error_message.kind_of?(Array)
-    if html_tag[0..5] == '<label'
-      html_tag.html_safe
-    else
-      %(#{html_tag}<span class="help-inline error">#{instance.error_message.join(',')}</span>).html_safe
-    end
+  message = if instance.error_message.respond_to? :join
+    instance.error_message.join(', ')
   else
-    if html_tag[0..5] == '<label'
-      html_tag.html_safe
-    else
-      %(#{html_tag}<span class="help-inline error">#{instance.error_message}</span>).html_safe
-    end
+    instance.error_message
+  end
+
+  if html_tag =~ /class="checkbox"/ || (!(html_tag =~ /type="checkbox"/) && !(html_tag =~ /label/))
+    %(#{html_tag}<span class="help-block help-error">⤷ #{message}</span>).html_safe
+  else
+    html_tag.html_safe
   end
 end
