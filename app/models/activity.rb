9d823249398c829a60b41bbe9093976be65d946a
Dani <dani@shiryu.home>
2013-08-13 23:45:08 +0100

    Refactor ordering into a module

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,9 @@
 class Activity < ActiveRecord::Base
+
+  include Ordenable
+
   validates :name, :presence => true
   validates :color, :format => /\A#[0-9A-Fa-f]{6}\z/
-  validates :position, :numericality => true, :uniqueness => true
-  
+
   has_many :events, :dependent => :destroy
 end
---
+++ app/models/ordenable.rb
@@ -0,0 +1,32 @@
+module Ordenable
+
+  def self.included(by)
+    by.class_eval do
+      before_save :set_position
+      after_save :reorder
+      validate :position_is_multiple_of_five, :on => :update
+    end
+  end
+
+  private
+
+  def position_is_multiple_of_five
+    if position.to_i % 5 != 0
+      errors.add(:position, 'should be multiple of 5')
+    end
+  end
+
+  def set_position
+    if position.nil?
+      self.position = self.class.select('MAX(position) AS max').order(nil).first.max.to_i + 10
+    end
+  end
+
+  def reorder
+    new_position = 10
+    self.class.order('position').each do |i|
+      i.update_column :position, new_position
+      new_position += 10
+    end
+  end
+end
