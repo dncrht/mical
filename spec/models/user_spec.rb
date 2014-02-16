24f840d8f4c3400f8189c1689a3d6a186c63a4d6
Dani <daniel.cr.ht@gmail.com>
2014-02-16 21:34:57 +0000

    Refactor User specs for readability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,35 +2,58 @@ require 'spec_helper'
 
 describe User do
 
-  let(:user) { FactoryGirl.create(:user) }
+  describe '#valid?' do
+    subject { FactoryGirl.build(:user, params) }
 
-  it 'has a valid factory' do
-    user.should be_valid
+    context 'default factory' do
+      let(:params) { nil }
+
+      it { should be_valid }
+    end
+
+    context 'without email' do
+      let(:params) { {email: nil} }
+
+      it { should_not be_valid }
+    end
+
+    context 'with invalid email' do
+      let(:params) { {email: 'nil@'} }
+
+      it { should_not be_valid }
+    end
+
+    context 'without password' do
+      let(:params) { {password: nil} }
+
+      it { should_not be_valid }
+    end
+
+    context 'is invalid if the email exists' do
+      let(:params) { {password: nil} }
+      let!(:existing_user) { FactoryGirl.create(:user, email: subject.email) }
+
+      it { should_not be_valid }
+      it { should have(1).error_on(:email) }
+    end
   end
 
-  it 'is invalid without email, or invalid email' do
-    FactoryGirl.build(:user, email: nil).should_not be_valid
+  describe '#save' do
+    subject { FactoryGirl.create(:user) }
 
-    FactoryGirl.build(:user, email: 'nil@').should_not be_valid
+    context 'accept a blank password if the user exists' do
+      before { subject.password = '' }
+
+      it { should be_valid }
+      it { expect(subject.save).to be_true }
+    end
   end
 
-  it 'is invalid without password' do
-    FactoryGirl.build(:user, password: nil).should_not be_valid
-  end
+  describe '#destroy' do
+    subject { FactoryGirl.create(:user) }
 
-  it 'accept a blank password if the user exists' do
-    user.password = ''
-    user.should be_valid
-    user.save.should be_true
-  end
-
-  it 'is invalid if the email exists' do
-    new_user = FactoryGirl.build(:user, email: user.email)
-    new_user.should_not be_valid
-    new_user.should have(1).error_on(:email)
-  end
-
-  it "can't delete the last admin" do
-    expect { user.destroy }.to raise_error 'Must be at least one admin'
+    context "can't delete the last admin" do
+      it { expect { subject.destroy }.to raise_error 'Must be at least one admin' }
+    end
   end
 end
