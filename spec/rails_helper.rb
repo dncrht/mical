69d0f5234e82bb95fabefcccb9e35c5296ae3c98
Dani <daniel.cr.ht@gmail.com>
2016-04-24 13:06:04 +0100

    Configure database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ require 'capybara/rails'
 require 'clearance/testing'
 require 'factory_girl_rails'
 
+require 'database_cleaner'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -23,10 +25,21 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :deletion
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
