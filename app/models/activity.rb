7d14c2ff957e84627c9c3bc331ebdbb63095dec9
Dani <daniel.cr.ht@gmail.com>
2017-05-12 10:14:25 +0200

    Count activities in legend

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,4 +5,15 @@ class Activity < ApplicationRecord
   validates :color, format: /\A#[0-9A-Fa-f]{6}\z/
 
   has_many :events, dependent: :destroy
+
+  def count_during_year(year)
+    Event
+      .where(
+        'activity_id = ? AND day >= ? AND day <= ?',
+        id,
+        Date.new(year, 1, 1),
+        Date.new(year, 12, 31)
+      )
+      .count
+  end
 end
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -43,4 +43,14 @@ RSpec.describe Activity do
   end
 
   it_should_behave_like 'sortable'
+
+  describe '#count_during_year' do
+    it 'counts the number of activities the given year' do
+      activity = create(:activity)
+      create(:event, day: '2000-01-01', activity: activity)
+      create(:event, day: '2000-12-31', activity: activity)
+
+      expect(activity.count_during_year(2000)).to eq 2
+    end
+  end
 end
