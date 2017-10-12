92301dd141bb1a8fa2bc732cdfe6f0cbd0e3ddcd
Dani <daniel.cr.ht@gmail.com>
2017-10-12 20:33:44 +0200

    Provide a way to render these applets easily from HTML and JS views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,20 +14,25 @@ module ApplicationHelper
     end.join("\n")
   end
 
-  def react_component(name, options = {})
+  def react_applet(name, options = {})
     @js = {} if @js.blank?
 
-    mount_point = "react_component#{rand(100)}"
+    mount_point = "react-applet#{rand(100)}"
 
     @js[mount_point] = {name: name, options: options}
 
-    content_tag(:div, nil, id: mount_point)
+    content_tag(:div, nil, id: "js-#{mount_point}")
   end
 
-  def render_react_components
+  def render_react_applet(name, options, mount_point = nil)
+    mount_point = mount_point || name.sub('Applet', '').downcase
+    "renderApplet('js-#{mount_point}', {applet: #{name}, initialModel: #{options.to_json}});".html_safe
+  end
+
+  def render_react_applets
     return if @js.blank?
     components = @js.map do |mount_point, component|
-      "renderApplet('#{mount_point}', {applet: #{component[:name]}, initialModel: #{component[:options].to_json}});"
+      render_react_applet component[:name], component[:options], mount_point
     end
 
     content_tag(:script, components.join("\n").html_safe)
