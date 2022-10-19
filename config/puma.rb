ba7a524fe420464b5b92025684539b30c0450bc5
Daniel Cruz <daniel@creditspring.co>
2022-10-19 08:23:39 +0200

    No port on production (only socket) to allow several apps on same server without PORT fiddling

--- config/puma.rb
+++ config/puma.rb
@@ -8,19 +8,23 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+env = ENV.fetch('RAILS_ENV') { 'development' }
+# Specifies the `environment` that Puma will run in.
+#
+environment env
+
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 3600 if env == 'development'
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
-bind "unix://tmp/sockets/puma.sock"
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+if env == 'production'
+  bind 'unix://tmp/sockets/puma.sock'
+else
+  port ENV.fetch("PORT") { 3000 }
+end
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
