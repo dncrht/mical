fe06ece2627e565ecc2f9a68c9ca111a5a745406
Daniel Cruz <daniel@creditspring.co>
2023-08-27 11:26:39 +0200

    Smarter activity management

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,16 +8,15 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.create(event_params.to_h.slice!('activity_id'))
-    create_event_activities if @event.id
+    @event.reload
 
     render 'show'
   end
 
   def update
     @event = Event.find_by(id: params[:id])
-    @event.update(event_params.to_h.slice!('activity_id'))
-    @event.event_activities.destroy_all
-    create_event_activities
+    @event.update(event_params)
+    @event.reload
 
     render 'show'
   end
@@ -41,12 +40,5 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit!
-  end
-
-  def create_event_activities
-    event_params['activity_id'].each do |id|
-      next if id.blank?
-      @event.event_activities.create activity_id: id
-    end
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,17 +1,21 @@
 class Event < ApplicationRecord
   TOP = 10
 
+  attr_accessor :activities_ids
+
   validates :day, :description, presence: true
   validates :day, uniqueness: true
   validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: TOP}, allow_nil: true
+  validate :activities_present
 
   has_many :event_activities
   has_many :activities, through: :event_activities
-  #accepts_nested_attributes_for :event_activities
 
   has_many :photos
 
   scope :in_year, ->(year) { where('day >= ? AND day <= ?', "#{year}-01-01", "#{year}-12-31").order('day') }
+
+  after_commit :manage_event_activities
 
   def self.first_year
     Event.first.nil? ? Date.current.year : Event.first.day.year
@@ -19,5 +23,22 @@ class Event < ApplicationRecord
 
   def self.to_h(events)
     Hash[*events.map { |event| [event.day.to_s, event] }.flatten] #http://snippets.dzone.com/posts/show/302
+  end
+
+  def activities_present
+    errors.add(:activities_ids, 'must select at least one') unless activities_ids.any?(&:present?)
+  end
+
+  def manage_event_activities
+    chosen_activities = activities_ids.select(&:present?).map(&:to_i).sort
+    existing_activities = activities.map(&:id).sort
+
+    return if chosen_activities == existing_activities
+
+    event_activities.where(activity_id: existing_activities - chosen_activities).destroy_all
+
+    (chosen_activities - existing_activities).each do |id|
+      event_activities.create activity_id: id
+    end
   end
 end
